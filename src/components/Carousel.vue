73d5cf732324fab8fc15e03c2ded865bd92fbefd
Radmir2015 <ya.radmir2015@yandex.ru>
2020-10-08 22:09:19 +0400

    [add] flat prop and show-arrows if needed

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,10 +1,10 @@
 <template>
-    <v-carousel :light="!$vuetify.theme.isDark" cycle height="100%" hide-delimiter-background hide-delimiters>
+    <v-carousel :light="!$vuetify.theme.isDark" cycle height="100%" hide-delimiter-background hide-delimiters :show-arrows="amountSliderPages > 1">
       <v-carousel-item v-for="index in arr(amountSliderPages)" :key="index">
         <v-sheet height="100%">
           <v-row justify="center" align="center" class="fill-height">
             <v-col v-bind="sliderItemsBreakpoints" v-for="ind in arr(Math.min(itemsPerSlide, items.length - index * itemsPerSlide))" :key="ind">
-              <v-card class="text-center">
+              <v-card class="text-center" :flat="flat">
                 <v-img :src="items[index * itemsPerSlide + ind].photo" :aspect-ratio="aspectRatio" :style="avatar ? imageStyle : ''" :contain="avatar"></v-img>
                 <v-card-title v-if="items[index * itemsPerSlide + ind].name" class="text-subtitle-1 font-weight-bold justify-center text-truncate">{{ items[index * itemsPerSlide + ind].name }}</v-card-title>
                 <v-card-subtitle v-if="items[index * itemsPerSlide + ind].exp">{{ items[index * itemsPerSlide + ind].exp }}</v-card-subtitle>
@@ -18,7 +18,7 @@
 
 <script>
 export default {
-    props: ['items', 'sliderItemsBreakpoints', 'aspectRatio', 'avatar', 'imageStyle'],
+    props: ['items', 'sliderItemsBreakpoints', 'aspectRatio', 'avatar', 'imageStyle', 'flat'],
     computed: {
       itemsPerSlide() {
         const breakpoint = {"xs": "cols"}[this.$vuetify.breakpoint.name]||this.$vuetify.breakpoint.name
